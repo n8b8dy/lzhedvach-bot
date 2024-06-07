e1b883f39432dfee74cd2c454655a83fbefa549e
n8b8dy <ruslanchernomorski@gmail.com>
2024-06-07 13:18:43 +0300

    Fix: Use AllowLzhedvachOnly middleware instead of WhiteList one

--- functionality/lzhedvacher/group.go
+++ functionality/lzhedvacher/group.go
@@ -1,21 +1,10 @@
 package lzhedvacher
 
 import (
-	"log"
-	"os"
-	"strconv"
-
 	tele "gopkg.in/telebot.v3"
-	"gopkg.in/telebot.v3/middleware"
 )
 
 func CreateGroup(b *tele.Bot) {
-	lzhedvachID, err := strconv.ParseInt(os.Getenv("LZHEDVACH_ID"), 10, 64)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
 	group := b.Group()
-	group.Use(middleware.Whitelist(lzhedvachID))
+	group.Use(AllowLzhedvachOnly())
 }
